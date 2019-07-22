d681c3abce2e1d844f805b784320ac5f1f27a3ea
Kevin Hallock <hallock@ucar.edu>
2019-07-22 17:03:04 -0600

    Added docstring for geocat.comp.linint2

--- src/geocat/comp/__init__.py
+++ src/geocat/comp/__init__.py
@@ -3,7 +3,97 @@ import numpy as np
 import xarray as xr
 from dask.array.core import map_blocks
 
-def linint2(fi, xo, yo, icycx, xmsg=None, iopt=0, meta=False, xi=None, yi=None):
+def linint2(fi, xo, yo, icycx, xmsg=None, meta=False, xi=None, yi=None):
+    """Return a 2-dimensionally linearly interpolated grid.
+
+    Args:
+
+        fi (:class:`xarray.DataArray` or :class:`numpy.ndarray`):
+            Geopotential height in [m] with the rightmost dimensions being
+            bottom_top x south_north x west_east.
+
+            Note:
+
+                This variable must be
+                supplied as a :class:`xarray.DataArray` in order to copy the
+                dimension names to the output.  Otherwise, default names will
+                be used.
+
+        xo (:class:`xarray.DataArray` or :class:`numpy.ndarray`):
+            A one-dimensional array that specifies the X coordinates of the
+            return array. It must be strictly monotonically increasing, but
+            may be unequally spaced.
+
+            For geo-referenced data, xo is generally the longitude array.
+
+        yo (:class:`xarray.DataArray` or :class:`numpy.ndarray`):
+            A one-dimensional array that specifies the Y coordinates of the
+            return array. It must be strictly monotonically increasing, but
+            may be unequally spaced.
+
+            For geo-referenced data, yo is generally the latitude array.
+
+        icycx (:obj:`bool`):
+            An option to indicate whether the rightmost dimension of fi is
+            cyclic. This should be set to True only if you have global data,
+            but your longitude values don't quite wrap all the way around the
+            globe. For example, if your longitude values go from, say, -179.75
+            to 179.75, or 0.5 to 359.5, then you would set this to True.
+
+        meta (:obj:`bool`):
+            Set to False to disable metadata; default is True.
+
+        xi (:class:`numpy.ndarray`):
+            An array that specifies the X coordinates of the fi array. Most
+            frequently, this is a 1D strictly monotonically increasing array
+            that may be unequally spaced. In some cases, xi can be a multi-
+            dimensional array (see next paragraph). The rightmost dimension
+            (call it nxi) must have at least two elements, and is the last
+            (fastest varying) dimension of fi.
+
+            If xi is a multi-dimensional array, then each nxi subsection of xi
+            must be strictly monotonically increasing, but may be unequally
+            spaced. All but its rightmost dimension must be the same size as
+            all but fi's rightmost two dimensions.
+
+            For geo-referenced data, xi is generally the longitude array.
+
+            Note:
+                If left unspecified, the rightmost coordinate dimension of fi
+                will be used. This parameter must be specified as a keyword
+                argument.
+
+        yi (:class:`numpy.ndarray`):
+            An array that specifies the Y coordinates of the fi array. Most
+            frequently, this is a 1D strictly monotonically increasing array
+            that may be unequally spaced. In some cases, yi can be a multi-
+            dimensional array (see next paragraph). The rightmost dimension
+            (call it nyi) must have at least two elements, and is the second-
+            to-last dimension of fi.
+
+            If yi is a multi-dimensional array, then each nyi subsection of yi
+            must be strictly monotonically increasing, but may be unequally
+            spaced. All but its rightmost dimension must be the same size as
+            all but fi's rightmost two dimensions.
+
+            For geo-referenced data, yi is generally the latitude array.
+
+            Note:
+                If left unspecified, the second-to-rightmost coordinate
+                dimension of fi will be used. This parameter must be specified
+                as a keyword argument.
+
+    Returns:
+
+        :class:`xarray.DataArray`: The interpolated grid. If the *meta*
+        parameter is True, then the result will include named dimensions
+        matching the input array. The returned value will have the same
+        dimensions as fi, except for the rightmost two dimensions which
+        will have the same dimension sizes as the lengths of yo and xo.
+        The return type will be double if fi is double, and float otherwise. 
+
+    """
+
     if xmsg is None:
         xmsg = _ncomp.dtype_default_fill[fi.dtype]
 
