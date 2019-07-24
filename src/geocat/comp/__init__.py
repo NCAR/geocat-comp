f14905e73159ba11e9ad9ef82215e244af44b644
Kevin Hallock <hallock@ucar.edu>
2019-07-24 14:41:24 -0600

    Detect edge cases for linint2 input types
    Ensure fi is an xarray.DataArray.
    
    Extract xi, yi, xo, and yo numpy arrays if they come in as xarray.DataArray.
    
    Determine if fi_xr.data is dask array, don't call map_blocks if not.

--- src/geocat/comp/__init__.py
+++ src/geocat/comp/__init__.py
@@ -1,6 +1,7 @@
 from . import _ncomp
 import numpy as np
 import xarray as xr
+import dask.array as da
 from dask.array.core import map_blocks
 
 def linint2(fi, xo, yo, icycx, xmsg=None, meta=True, xi=None, yi=None):
@@ -140,20 +141,47 @@ def linint2(fi, xo, yo, icycx, xmsg=None, meta=True, xi=None, yi=None):
     if xmsg is None:
         xmsg = _ncomp.dtype_default_fill[fi.dtype]
 
+    if not isinstance(fi, xr.DataArray):
+        fi = xr.DataArray(fi)
+
     if xi is None:
         xi = fi.coords[fi.dims[-1]].values
+    elif isinstance(xi, xr.DataArray):
+        xi = xi.values
+
     if yi is None:
         yi = fi.coords[fi.dims[-2]].values
-    fi_data = fi.data
-    fo_chunks = list(fi.chunks)
-    fo_chunks[-2:] = (yo.shape, xo.shape)
-    fo = map_blocks(_ncomp._linint2, xi, yi, fi_data, xo, yo, icycx, xmsg, chunks=fo_chunks, dtype=fi.dtype, drop_axis=[fi.ndim-2, fi.ndim-1], new_axis=[fi.ndim-2, fi.ndim-1])
+    elif isinstance(yi, xr.DataArray):
+        yi = yi.values
 
-    result = fo.compute()
+    # ensure xo and yo are numpy.ndarrays
+    if isinstance(xo, xr.DataArray):
+        xo = xo.values
+    if isinstance(yo, xr.DataArray):
+        yo = yo.values
+
+    fi_data = fi.data
+
+    if isinstance(fi_data, da.Array):
+        fo_chunks = list(fi.chunks)
+        fo_chunks[-2:] = (yo.shape, xo.shape)
+        fo = map_blocks(_ncomp._linint2, xi, yi, fi_data, xo, yo, icycx, xmsg,
+                        chunks=fo_chunks, dtype=fi.dtype,
+                        drop_axis=[fi.ndim-2, fi.ndim-1],
+                        new_axis=[fi.ndim-2, fi.ndim-1])
+        result = fo.compute()
+    elif isinstance(fi_data, np.ndarray):
+        result = _ncomp._linint2(xi, yi, fi_data, xo, yo, icycx, xmsg)
+    else:
+        raise TypeError
 
     if meta:
-        coords = {k:v if k not in fi.dims[-2:] else (xo if k == fi.dims[-1] else yo) for (k, v) in fi.coords.items()}
-        result = xr.DataArray(result, attrs=fi.attrs, dims=fi.dims, coords=coords)
+        coords = {k:v if k not in fi.dims[-2:]
+                  else (xo if k == fi.dims[-1] else yo)
+                  for (k, v) in fi.coords.items()}
+
+        result = xr.DataArray(result, attrs=fi.attrs, dims=fi.dims,
+                              coords=coords)
     else:
         result = xr.DataArray(result)
 
