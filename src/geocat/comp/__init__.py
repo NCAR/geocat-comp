c6083ff22b2b0eb9beaf37f1cc26ddd0f2d21480
Kevin Hallock <hallock@ucar.edu>
2019-07-24 14:17:21 -0600

    Add linint2 usage example

--- src/geocat/comp/__init__.py
+++ src/geocat/comp/__init__.py
@@ -96,6 +96,33 @@ def linint2(fi, xo, yo, icycx, xmsg=None, meta=True, xi=None, yi=None):
         will have the same dimension sizes as the lengths of yo and xo.
         The return type will be double if fi is double, and float otherwise. 
 
+    Examples:
+
+        Example 1: Using linint2 with :class:`xarray.DataArray` input
+
+        .. code-block:: python
+
+            import numpy as np
+            import xarray as xr
+            import geocat.comp
+
+            fi_np = np.random.rand(30, 80)  # random 30x80 array
+
+            # xi and yi do not have to be equally spaced, but are in this example
+            xi = np.arange(80)
+            yi = np.arange(30)
+
+            # create target coordinate arrays, in this case using the same
+            # min/max values as xi and yi, but with different spacing
+            xo = np.linspace(xi.min(), xi.max(), 100)
+            yo = np.linspace(yi.min(), yi.max(), 50)
+
+            # create :class:`xarray.DataArray` and chunk it using the full shape of the original array
+            # note that xi and yi are attached as coordinate arrays
+            fi = xr.DataArray(fi_np, dims=['lat', 'lon'], coords={'lat': yi, 'lon': xi}).chunk(fi_np.shape)
+
+            fo = geocat.comp.linint2(fi, xo, yo, 0)
+
     """
 
     if xmsg is None:
