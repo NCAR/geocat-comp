fca63cd5f19709c3fb45e96d6da0aee32c6f1e0f
Kevin Hallock <hallock@ucar.edu>
2020-04-09 10:43:52 -0600

    Remove geocat-ncomp functionality
    Functions from geocat.ncomp are imported into the geocat.comp namespace
    by using "from geocat.ncomp import *" inside a try/except. If
    geocat-ncomp is not installed, then these functions will silently fail
    to import.

---
+++ geocat/comp/__init__.py
@@ -0,0 +1,45 @@
+from typing import Iterable
+
+from .version import __version__
+import numpy as np
+import xarray as xr
+import dask.array as da
+from dask.array.core import map_blocks
+
+from .polynomial import ndpolyfit, ndpolyval, detrend
+
+try:
+    from geocat.ncomp import *
+except ImportError:
+    pass
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
+class ChunkError(Error):
+    """Exception raised when a Dask array is chunked in a way that is
+    incompatible with an _ncomp function."""
+    pass
+
+
+class CoordinateError(Error):
+    """Exception raised when a GeoCAT-comp function is passed a NumPy array as
+    an argument without a required coordinate array being passed separately."""
+    pass
+
+class DimensionError(Error):
+     """Exception raised when the arguments of GeoCAT-comp functions argument
+     has a mismatch of the necessary dimensionality."""
+     pass
+
+class AttributeError(Error):
+     """Exception raised when the arguments of GeoCAT-comp functions argument
+     has a mismatch of attributes with other arguments."""
+     pass
+
+class MetaError(Error):
+     """Exception raised when the support for the retention of metadata is not
+     supported."""
+     pass
--- setup.py
+++ setup.py
@@ -6,32 +6,13 @@ except ImportError:
     from distutils.extension import Extension
 
 #import distutils.sysconfig
-from Cython.Build import cythonize
-import numpy
-import os
-import sys
 
-with open("src/geocat/comp/version.py") as f:
+with open("geocat/comp/version.py") as f:
     exec(f.read())
 
-SRC_DIR = os.path.dirname(os.path.abspath(__file__))
-PREFIX = os.path.normpath(sys.prefix)
-
-include_dirs = [os.path.join(PREFIX, 'include'), numpy.get_include()]
-
-extensions = [
-    Extension("geocat.comp._ncomp", ["src/geocat/comp/_ncomp.pyx"],
-        include_dirs=include_dirs,
-        libraries=["ncomp"],
-),
-]
 setup(
     name="geocat.comp",
-    ext_modules=cythonize(extensions,
-                          # help cythonize find my own .pxd files
-                          include_path=[os.path.join(SRC_DIR, "src/geocat/comp/_ncomp")]),
-    package_dir={'geocat': 'src/geocat', 'geocat.comp': 'src/geocat/comp'},
-    package_data={'geocat': ['__init__.pxd', 'comp/*.pxd']},
+    package_dir={'geocat': 'geocat', 'geocat.comp': 'geocat/comp'},
     namespace_packages=['geocat'],
     packages=["geocat", "geocat.comp"],
     version=__version__, 
