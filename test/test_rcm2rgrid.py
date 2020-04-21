f3798ef182e523200d15a6164381cafdc5d7ae8b
pilotchute <akootz@ucar.edu>
2020-04-21 15:30:40 -0600

    test case refactor done, __ncomp__.pyx needs to be looked at, and __init__.py needs to be un-PEP8ed
    Signed-off-by: pilotchute <akootz@ucar.edu>

--- test/test_rcm2rgrid.py
+++ test/test_rcm2rgrid.py
@@ -1,20 +1,20 @@
 import numpy as np
+import numpy.testing as nt
 import xarray as xr
-import geocat.comp
+import geocat.comp as gc
 
 import sys
 import time
-import pytest as pt
 import unittest as ut
 
 
 # nominal input
-fi = np.asarray(
+fi_nom = np.asarray(
     [1.870327, 1.872924, 2.946794, 1.98253, 1.353965, 0.8730035, 0.1410671, 1.877125, 1.931963, -0.1676207, 1.917912, 1.735453, -1.82497, 1.01385, 1.053591,
      1.754721, 1.177423, 0.381366, 2.015617, 0.4975608, 2.169137, 0.3293635, 0.6676366, 2.691788, 2.510986, 1.027274, 1.351906]).reshape((3, 3, 3))
 
 # nan input
-fi_nan = fi.copy()
+fi_nan = fi_nom.copy()
 fi_nan[0, 1, 1] = np.nan
 fi_nan[1, 1, 1] = np.nan
 fi_nan[2, 1, 1] = np.nan
@@ -32,63 +32,40 @@ lon2d = np.asarray([1, 1, 1, 2, 2, 2, 5, 5, 5]).reshape((3, 3))
 msg64 = fi_msg[1, 1, 1].astype(np.float64)
 msg32 = fi_msg[1, 1, 1].astype(np.float32)
 
-
-@pt.mark.skip(reason="Not a test.")
-def tests(fi, msg=None):
-    fo = geocat.comp.rcm2rgrid(lat2d, lon2d, fi, lat, lon, msg=msg)
-    return [fo]
-
-
-@pt.mark.skip(reason="Not a test.")
-def assertions(expected_results, results):
-    np.testing.assert_array_almost_equal(expected_results[0], results[0])
-
-
-fo_expected = np.asarray(
+fo_nom_expected = np.asarray(
     [1.870327, 1.98253, 0.1410671, 1.872924, 1.353965, 1.877125, 2.946794, 0.8730035, 1.931963, -0.1676207, -1.82497, 1.754721, 1.917912, 1.01385, 1.177423,
      1.735453, 1.053591, 0.381366, 2.015617, 0.3293635, 2.510986, 0.4975608, 0.6676366, 1.027274, 2.169137, 2.691788, 1.351906]).reshape((3, 3, 3))
-expected_results = [fo_expected]
 
-fo_expected_nan = np.array(
+fo_nan_expected = np.array(
     [1.870327, 1.98253, 0.1410671, 1.872924, 1.875024, 1.877125, 2.946794, 0.8730035, 1.931963, -0.1676207, -1.82497, 1.754721, 1.917912, 1.547667, 1.177423,
      1.735453, 1.053591, 0.381366, 2.015617, 0.3293635, 2.510986, 0.4975608, 0.7624173, 1.027274, 2.169137, 2.691788, 1.351906]).reshape((3, 3, 3))
-expected_results_nan = [fo_expected_nan]
 
-fo_expected_msg = np.array(
+fo_msg_expected = np.array(
     [1.870327, 1.98253, 0.1410671, 1.872924, 1.875024, 1.877125, 2.946794, 0.8730035, 1.931963, -0.1676207, -1.82497, 1.754721, 1.917912, 1.547667, 1.177423,
      1.735453, 1.053591, 0.381366, 2.015617, 0.3293635, 2.510986, 0.4975608, 0.7624173, 1.027274, 2.169137, 2.691788, 1.351906]).reshape((3, 3, 3))
-expected_results_msg = [fo_expected_msg]
 
 
 # run tests
-
-class Test_rcm2rgrid_float64(ut.TestCase):
+class Test_rcm2rgrid(ut.TestCase):
     """
     Test_rcm2rgrid_float64
     This unit test covers the nominal, nan, and msg cases of 64 bit float input for rcm2rgrid
     """
 
-    def test_rcm2rgrid_float64(self):
-        assertions(expected_results, tests(fi.astype(np.float64)))
+    def test_rcm2rgrid_float64_nom(self):
+        nt.assert_array_almost_equal(fo_nom_expected, gc.rcm2rgrid(lat2d, lon2d, fi_nom.astype(np.float64), lat, lon))
 
     def test_rcm2rgrid_float64_nan(self):
-        assertions(expected_results_nan, tests(fi_nan.astype(np.float64)))
+        nt.assert_array_almost_equal(fo_nan_expected, gc.rcm2rgrid(lat2d, lon2d, fi_nan.astype(np.float64), lat, lon))
 
     def test_rcm2rgrid_float64_msg(self):
-        assertions(expected_results_msg, tests(fi_msg.astype(np.float64), msg=msg64))
+        nt.assert_array_almost_equal(fo_msg_expected, gc.rcm2rgrid(lat2d, lon2d, fi_msg.astype(np.float64), lat, lon, msg=msg64))
 
-
-class Test_rcm2rgrid_float32(ut.TestCase):
-    """
-    Test_rcm2rgrid_float32
-    This unit test covers the nominal, nan, and msg cases of 32 bit float input for rcm2rgrid
-    """
-
-    def test_rcm2rgrid_float32(self):
-        assertions(expected_results, tests(fi.astype(np.float32)))
+    def test_rcm2rgrid_float32_nom(self):
+        nt.assert_array_almost_equal(fo_nom_expected, gc.rcm2rgrid(lat2d, lon2d, fi_nom.astype(np.float32), lat, lon))
 
     def test_rcm2rgrid_float32_nan(self):
-        assertions(expected_results_nan, tests(fi_nan.astype(np.float32)))
+        nt.assert_array_almost_equal(fo_nan_expected, gc.rcm2rgrid(lat2d, lon2d, fi_nan.astype(np.float32), lat, lon))
 
     def test_rcm2rgrid_float32_msg(self):
-        assertions(expected_results_msg, tests(fi_msg.astype(np.float32), msg=msg32))
+        nt.assert_array_almost_equal(fo_msg_expected, gc.rcm2rgrid(lat2d, lon2d, fi_msg.astype(np.float32), lat, lon, msg=msg32))
--- test/test_rgrid2rcm.py
+++ test/test_rgrid2rcm.py
@@ -1,20 +1,20 @@
 import numpy as np
+import numpy.testing as nt
 import xarray as xr
-import geocat.comp
+import geocat.comp as gc
 
 import sys
 import time
-import pytest as pt
 import unittest as ut
 
 
 # nominal input
-fi = np.asarray(
+fi_nom = np.asarray(
     [1.870327, 1.872924, 2.946794, 1.98253, 1.353965, 0.8730035, 0.1410671, 1.877125, 1.931963, -0.1676207, 1.917912, 1.735453, -1.82497, 1.01385, 1.053591,
      1.754721, 1.177423, 0.381366, 2.015617, 0.4975608, 2.169137, 0.3293635, 0.6676366, 2.691788, 2.510986, 1.027274, 1.351906]).reshape((3, 3, 3))
 
 # nan input
-fi_nan = fi.copy()
+fi_nan = fi_nom.copy()
 fi_nan[0, 1, 1] = np.nan
 fi_nan[1, 1, 1] = np.nan
 fi_nan[2, 1, 1] = np.nan
@@ -31,64 +31,40 @@ lon2d = np.asarray([1, 1, 1, 2, 2, 2, 5, 5, 5]).reshape((3, 3))
 
 msg64 = fi_msg[1, 1, 1].astype(np.float64)
 msg32 = fi_msg[1, 1, 1].astype(np.float32)
-
-
-@pt.mark.skip(reason="Not a test.")
-def tests(fi, msg=None):
-    fo = geocat.comp.rgrid2rcm(lat, lon, fi, lat2d, lon2d, msg=msg)
-    return [fo]
-
-
-@pt.mark.skip(reason="Not a test.")
-def assertions(expected_results, results):
-    np.testing.assert_array_almost_equal(expected_results[0], results[0])
-
-
-fo_expected = np.asarray(
+fo_nom_expected = np.asarray(
     [1.870327, 1.98253, 0.1410671, 1.872924, 1.353965, 1.877125, 2.946794, 0.8730035, 1.931963, -0.1676207, -1.82497, 1.754721, 1.917912, 1.01385, 1.177423,
      1.735453, 1.053591, 0.381366, 2.015617, 0.3293635, 2.510986, 0.4975608, 0.6676366, 1.027274, 2.169137, 2.691788, 1.351906]).reshape((3, 3, 3))
-expected_results = [fo_expected]
 
-fo_expected_nan = np.array(
+fo_nan_expected = np.array(
     [1.870327, 1.98253, 0.1410671, 1.872924, 1.486321, 1.877125, 2.946794, 0.8730035, 1.931963, -0.1676207, -1.82497, 1.754721, 1.917912, 0.9684872, 1.177423,
      1.735453, 1.053591, 0.381366, 2.015617, 0.3293635, 2.510986, 0.4975608, 1.7583, 1.027274, 2.169137, 2.691788, 1.351906]).reshape((3, 3, 3))
-expected_results_nan = [fo_expected_nan]
 
-fo_expected_msg = np.array(
+fo_msg_expected = np.array(
     [1.870327, 1.98253, 0.1410671, 1.872924, 1.486321, 1.877125, 2.946794, 0.8730035, 1.931963, -0.1676207, -1.82497, 1.754721, 1.917912, 0.9684872, 1.177423,
      1.735453, 1.053591, 0.381366, 2.015617, 0.3293635, 2.510986, 0.4975608, 1.7583, 1.027274, 2.169137, 2.691788, 1.351906]).reshape((3, 3, 3))
-expected_results_msg = [fo_expected_msg]
 
 
 # run tests
-
-class Test_rgrid2rcm_float64(ut.TestCase):
+class Test_rgrid2rcm(ut.TestCase):
     """
     Test_rgrid2rcm_float64
     This unit test covers the nominal, nan, and msg cases of 64 bit float input for rgrid2rcm
     """
 
-    def test_rgrid2rcm_float64(self):
-        assertions(expected_results, tests(fi.astype(np.float64)))
+    def test_rgrid2rcm_float64_nom(self):
+        nt.assert_array_almost_equal(fo_nom_expected, gc.rgrid2rcm(lat, lon, fi_nom.astype(np.float64), lat2d, lon2d))
 
     def test_rgrid2rcm_float64_nan(self):
-        assertions(expected_results_nan, tests(fi_nan.astype(np.float64)))
+        nt.assert_array_almost_equal(fo_nan_expected, gc.rgrid2rcm(lat, lon, fi_nan.astype(np.float64), lat2d, lon2d))
 
     def test_rgrid2rcm_float64_msg(self):
-        assertions(expected_results_msg, tests(fi_msg.astype(np.float64), msg=msg64))
+        nt.assert_array_almost_equal(fo_msg_expected, gc.rgrid2rcm(lat, lon, fi_msg.astype(np.float64), lat2d, lon2d, msg=msg64))
 
-
-class Test_rgrid2rcm_float32(ut.TestCase):
-    """
-    Test_rgrid2rcm_float32
-    This unit test covers the nominal, nan, and msg cases of 32 bit float input for rgrid2rcm
-    """
-
-    def test_rgrid2rcm_float32(self):
-        assertions(expected_results, tests(fi.astype(np.float32)))
+    def test_rgrid2rcm_float32_nom(self):
+        nt.assert_array_almost_equal(fo_nom_expected, gc.rgrid2rcm(lat, lon, fi_nom.astype(np.float32), lat2d, lon2d))
 
     def test_rgrid2rcm_float32_nan(self):
-        assertions(expected_results_nan, tests(fi_nan.astype(np.float32)))
+        nt.assert_array_almost_equal(fo_nan_expected, gc.rgrid2rcm(lat, lon, fi_nan.astype(np.float32), lat2d, lon2d))
 
     def test_rgrid2rcm_float32_msg(self):
-        assertions(expected_results_msg, tests(fi_msg.astype(np.float32), msg=msg32))
+        nt.assert_array_almost_equal(fo_msg_expected, gc.rgrid2rcm(lat, lon, fi_msg.astype(np.float32), lat2d, lon2d, msg=msg32))
