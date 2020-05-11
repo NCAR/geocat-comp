b24e9954e0ec7c74e8be52f66dc78e70cfae256b
bonnland <bonnland@ucar.edu>
2020-05-11 16:41:39 -0600

    Generalize time coordinate name

--- src/geocat/comp/month_to_season.py
+++ src/geocat/comp/month_to_season.py
@@ -1,6 +1,6 @@
 
 
-def month_to_season(xMon, season):
+def month_to_season(xMon, season, time_coord_name='time'):
     """ This function takes an xarray dataset containing monthly data spanning years and
         returns a dataset with one sample per year, for a specified three-month season.
 
@@ -12,10 +12,10 @@ def month_to_season(xMon, season):
         is dropped.  For example, if the monthly data's time range is [Jan-2000, Dec-2003] and the season is "DJF", the
         seasonal mean computed from the single month of Dec-2003 is dropped.
     """
-    mod_check(xMon.time.size, 12)
+    mod_check(xMon[time_coord_name].size, 12)
 
-    startDate = xMon.time[0]
-    endDate = xMon.time[-1]
+    startDate = xMon[time_coord_name][0]
+    endDate = xMon[time_coord_name][-1]
     seasons_pd = {'DJF': ('QS-DEC', 1), 'JFM': ('QS-JAN', 2), 'FMA': ('QS-FEB', 3), 'MAM': ('QS-MAR', 4),
                   'AMJ': ('QS-APR', 5), 'MJJ': ('QS-MAY', 6), 'JJA': ('QS-JUN', 7), 'JAS': ('QS-JUL', 8),
                   'ASO': ('QS-AUG', 9), 'SON': ('QS-SEP', 10), 'OND': ('QS-OCT', 11), 'NDJ': ('QS-NOV', 12)}
