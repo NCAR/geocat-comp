1d7a9d3941eff0197c6c2684d8f35d1c46c68d87
pilotchute <akootz@ucar.edu>
2020-06-11 09:52:56 -0600

    updated circleci to build with libncomp
    Signed-off-by: pilotchute <akootz@ucar.edu>

--- .circleci/install-circle.sh
+++ .circleci/install-circle.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 
 conda config --set always_yes true --set changeps1 false --set quiet true
 conda config --add channels conda-forge
-conda create --clone ${NCOMP_ENV_NAME} --name ${ENV_NAME}
+conda create --clone ${LIBNCOMP_ENV_NAME} --name ${ENV_NAME}
 conda env update -f .circleci/environment-dev-$(uname)-${PYTHON}.yml --name ${ENV_NAME} --quiet
 conda env list
 source activate ${ENV_NAME}
