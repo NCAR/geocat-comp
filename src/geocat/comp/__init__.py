bc3cf76f7d8cbdc60a47048e8aea088de96920ff
pilotchute <akootz@ucar.edu>
2020-07-24 14:51:45 -0600

    comments on namespace manipulation
    Signed-off-by: pilotchute <akootz@ucar.edu>

--- src/geocat/comp/__init__.py
+++ src/geocat/comp/__init__.py
@@ -1,9 +1,13 @@
-from .errors import (Error, AttributeError, ChunkError, CoordinateError,
-                     DimensionError, MetaError)
+# move functions into geocat.comp namespace
+from .errors import (
+    Error, AttributeError, ChunkError, CoordinateError,
+    DimensionError, MetaError)
 from .polynomial import (ndpolyfit, ndpolyval, detrend, isvector)
 from .version import __version__
 
+
+# bring all functions from geocat.ncomp into the geocat.comp namespace
 try:
-    from geocat.ncomp import *
+    from geocat.ncomp import *  # bring all functions from geocat.ncomp into the geocat.comp namespace
 except ImportError:
     pass
