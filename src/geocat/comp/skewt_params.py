3d03ba6f61f5f2addcb5a811e3d39592d47826ca
michaelavs <michaela.sizemore@gmail.com>
2021-01-05 12:13:11 -0700

    Adding print statements for calculation clarity

--- src/geocat/comp/skewt_params.py
+++ src/geocat/comp/skewt_params.py
@@ -24,7 +24,7 @@ tc = (ds[5].values + 2)*units.degC # Temperature [C]
 tdc = (ds[9].values + 2)*units.degC  # Dew pt temp  [C]
 ta = mpcalc.parcel_profile(p, tc[0], tdc[0]) # Parcel profile
 tac = (ta.magnitude - 273.15)*units.degC # Parcel temp in C
-
+print("The parcel temp at 500mb is", tac[11])
 ##############################################################################
 
 # CAPE and CIN
@@ -41,15 +41,18 @@ lcl = mpcalc.lcl(p[0], tc[0], tdc[0])
 
 
 # Shox/Stability
+# Calculate parcel temp when raised dry adiabatically from surface to lcl
 dl = mpcalc.dry_lapse(p[2], tac[0], p[0])
 dl = (dl.magnitude - 273.15)*units.degC # Change units to C
 # print((dl))
 
+# Calculate parcel temp when raised moist adiabatically from lcl to 500mb
 ml = mpcalc.moist_lapse(p[11], dl, p[2]) 
 # print(ml)
 
+# Define environmental temp at 500mb
 ttop = tc[11]
-# print(ttop)
+print("The environmental temp at 500mb is", ttop)
 
 shox = ttop - ml
-print(shox)
+print("The calculated value for Showalter index is", shox)
