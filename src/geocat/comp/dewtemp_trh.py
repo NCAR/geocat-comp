c3e31a35243df8722ce96ad4295ad40f774fa94f
anissa111 <anissaz@ucar.edu>
2021-01-26 13:20:40 -0700

    update to doctring format

--- src/geocat/comp/dewtemp_trh.py
+++ src/geocat/comp/dewtemp_trh.py
@@ -6,20 +6,21 @@ from dask.array.core import map_blocks
 
 def dewtemp_trh(temperature, relative_humidity):
     """ This function calculates the dew point temperature given temperature and relative humidity
-            using equations from John Dutton's "Ceaseless Wind" (pp 273-274)
+        using equations from John Dutton's "Ceaseless Wind" (pp 273-274)
 
-            Parameters
-            ----------
-            temperature : numpy.ndarray, xr.DataArray, list, or float
-                Temperature in K
-            relative_humidity : numpy.ndarray, xr.DataArray, list, or float
-                Relative humidity. Must be the same dimensions as temperature
+            Args:
+
+                temperature (:class:`numpy.ndarray`, :class:`xr.DataArray`, :obj:`list`, or :obj:`float`):
+                    Temperature in K
+
+                relative_humidity (:class:`numpy.ndarray`, :class:`xr.DataArray`, :obj:`list`, or :obj:`float`):
+                    Relative humidity. Must be the same dimensions as temperature
 
 
-            Returns
-            -------
-            tdk : numpy.ndarray, xr.DataArray, list, or float
-                Relative humidity. Same size as input variable tk
+            Returns:
+
+                dew_pnt_temp (:class:`numpy.ndarray`, :class:`xr.DataArray`, :obj:`list`, or :obj:`float`):
+                    Dewpoint temperature in Kelvin. Same size as input variable temperature
     """
 
     # make sure the input arrays are of the same size
@@ -41,30 +42,30 @@ def dewtemp_trh(temperature, relative_humidity):
         relative_humidity = xr.DataArray(relative_humidity)
         relative_humidity = da.from_array(relative_humidity, chunks="auto")
 
-    tdk = map_blocks(_dewtemp, temperature, relative_humidity)
-    tdk = tdk.compute()
+    dew_pnt_temp = map_blocks(_dewtemp, temperature, relative_humidity)
+    dew_pnt_temp = dew_pnt_temp.compute()
 
     # tdk = _dewtemp(temperature, relative_humidity)
-    return tdk
+    return dew_pnt_temp
 
 
 def _dewtemp(tk, rh):
     """ This function calculates the dew point temperature given temperature and relative humidity
         using equations from John Dutton's "Ceaseless Wind" (pp 273-274)
 
-        Parameters
-        ----------
-        tk : numpy.ndarray, xr.DataArray, list, or float
-            Temperature in K
-        rh : numpy.ndarray, xr.DataArray, list, or float
-            Relative humidity
+            Args:
+
+                tk (:class:`numpy.ndarray`, :class:`xr.DataArray`, :obj:`list`, or :obj:`float`):
+                    Temperature in K
+
+                rh (:class:`numpy.ndarray`, :class:`xr.DataArray`, :obj:`list`, or :obj:`float`):
+                    Relative humidity. Must be the same dimensions as temperature
 
 
-        Returns
-        -------
-        tdk : numpy.ndarray, xr.DataArray, list, or float
-            Relative humidity
+            Returns:
 
+                tdk (:class:`numpy.ndarray`, :class:`xr.DataArray`, :obj:`list`, or :obj:`float`):
+                    Dewpoint temperature in Kelvin. Same size as input variable temperature
     """
 
     gc = 461.5  # gas constant for water vapor [j/{kg-k}]
