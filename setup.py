20ebb2916cc4d6d16cb53351b0fb2a8f7db4eed0
pilotchute <akootz@ucar.edu>
2021-02-05 13:02:42 -0700

    ready to test conda-build
    Signed-off-by: pilotchute <akootz@ucar.edu>

--- setup.py
+++ setup.py
@@ -1,22 +1,17 @@
-try:
-    from setuptools import setup
-    from setuptools import Extension
-except ImportError:
-    from distutils.core import setup
-    from distutils.extension import Extension
+#''' setup.py is needed, but only to make namespaces happen,
+version = '2021.01.1'
 
-#import distutils.sysconfig
+from setuptools import setup, find_packages
+from pathlib import Path
 
-with open("src/geocat/comp/version.py") as f:
-    exec(f.read())
-
-setup(name="geocat.comp",
-      package_dir={
-          '': 'src',
-          'geocat': 'src/geocat',
-          'geocat.comp': 'src/geocat/comp'
-      },
-      namespace_packages=['geocat'],
-      packages=["geocat", "geocat.comp"],
-      version=__version__,
-      install_requires=['numpy', 'xarray', 'dask[complete]'])
+setup(
+    name="geocat.comp",
+    version=version,
+    package_dir={
+      '': 'src',
+      'geocat': 'src/geocat',
+      'geocat.comp': 'src/geocat/comp'
+    },
+    namespace_packages=['geocat'],
+    packages=["geocat", "geocat.comp"],
+)
--- src/geocat/comp/__init__.py
+++ src/geocat/comp/__init__.py
@@ -2,10 +2,8 @@
 from .errors import (Error, AttributeError, ChunkError, CoordinateError,
                      DimensionError, MetaError)
 from .polynomial import (ndpolyfit, ndpolyval, detrend, isvector)
-from .version import __version__
-
 # bring all functions from geocat.ncomp into the geocat.comp namespace
 try:
-    from geocat.ncomp import *
+    from geocat.f2py import *
 except ImportError:
     pass
