64353bfa93b408142ff5faa311d09c4400958e54
pilotchute <akootz@ucar.edu>
2021-02-11 15:52:34 -0700

    version now populates from meta.yaml
    Signed-off-by: pilotchute <akootz@ucar.edu>

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
-#''' setup.py is needed, but only to make namespaces happen,
-version = '2021.01.1'
-
+#''' setup.py is needed, but only to make namespaces happen
 from setuptools import setup, find_packages
 from pathlib import Path
 
+#''' moved into function, can now be used other places
+def version():
+    for line in open('meta.yaml').readlines():
+        index = line.find('version')
+        if index > -1:
+            return line[index+8:].replace('\'','' ).strip()
+
 setup(
-    name="geocat.comp",
-    version=version,
+    name='geocat.comp',
+    version=version(),
     package_dir={
         '': 'src',
         'geocat': 'src/geocat',
         'geocat.comp': 'src/geocat/comp'
     },
     namespace_packages=['geocat'],
-    packages=["geocat", "geocat.comp"],
+    packages=['geocat', 'geocat.comp'],
 )
