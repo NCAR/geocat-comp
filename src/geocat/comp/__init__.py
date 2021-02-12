dc2e43ed59182541dd0dfc2a5a85bbdeca87ac34
erogluorhan <erogluorhan@gmail.com>
2021-02-11 18:02:24 -0700

    Fixed broken _inint.py after version.py removal

--- src/geocat/comp/__init__.py
+++ src/geocat/comp/__init__.py
@@ -3,9 +3,8 @@ from .climatology import climatology, anomaly
 from .errors import (Error, AttributeError, ChunkError, CoordinateError,
                      DimensionError, MetaError)
 from .polynomial import (ndpolyfit, ndpolyval, detrend, isvector)
-from .version import __version__
 
-# bring all functions from geocat.ncomp into the geocat.comp namespace
+# bring all functions from geocat.f2py into the geocat.comp namespace
 try:
     from geocat.f2py import *
 except ImportError:
