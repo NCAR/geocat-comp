57e633065044f7bd4f68189d45c5b23eeef297cb
Anderson Banihirwe <axbanihirwe@ualr.edu>
2021-02-11 17:44:17 -0700

    Add usage examples and update docs

--- src/geocat/comp/climatology.py
+++ src/geocat/comp/climatology.py
@@ -10,9 +10,7 @@ _FREQUENCIES = {"day", "month", "year", "season"}
 
 def _find_time_invariant_vars(dset, time_coord_name):
     if isinstance(dset, xr.Dataset):
-        return [
-            v for v in dset.variables if time_coord_name not in dset[v].dims
-        ]
+        return [v for v in dset.variables if time_coord_name not in dset[v].dims]
     return
 
 
@@ -21,8 +19,8 @@ def _contains_datetime_like_objects(d_arr):
     np.datetime64, or cftime.datetime)
     """
     return np.issubdtype(
-        d_arr.dtype,
-        np.datetime64) or xr.core.common.contains_cftime_datetimes(d_arr)
+        d_arr.dtype, np.datetime64
+    ) or xr.core.common.contains_cftime_datetimes(d_arr)
 
 
 def _validate_freq(freq):
@@ -57,12 +55,9 @@ def _setup_clim_anom_input(dset, freq, time_coord_name):
 
 
 def climatology(
-        dset: typing.Union[xr.DataArray, xr.Dataset],
-        freq: str,
-        time_coord_name: str = None) -> typing.Union[xr.DataArray, xr.Dataset]:
-    """
-
-    Compute climatologies for a specified time frequency
+    dset: typing.Union[xr.DataArray, xr.Dataset], freq: str, time_coord_name: str = None
+) -> typing.Union[xr.DataArray, xr.Dataset]:
+    """Compute climatologies for a specified time frequency.
 
     Parameters
     ----------
@@ -84,9 +79,55 @@ def climatology(
     -------
     computed_dset : xr.Dataset, xr.DataArray
        The computed climatology data
+
+    Examples
+    --------
+    >>> import xarray as xr
+    >>> import pandas as pd
+    >>> import numpy as np
+    >>> import geocat.comp
+    >>> dates = pd.date_range(start="2000/01/01", freq="M", periods=24)
+    >>> ts = xr.DataArray(np.arange(24).reshape(24, 1, 1), dims=["time", "lat", "lon"], coords={"time": dates})
+    >>> ts
+    <xarray.DataArray (time: 24, lat: 1, lon: 1)>
+    array([[[ 0]],
+
+        [[ 1]],
+
+        [[ 2]],
+    ...
+        [[21]],
+
+        [[22]],
+
+        [[23]]])
+    Coordinates:
+    * time     (time) datetime64[ns] 2000-01-31 2000-02-29 ... 2001-12-31
+    Dimensions without coordinates: lat, lon
+    >>> geocat.comp.climatology(ts, 'year')
+    <xarray.DataArray (year: 2, lat: 1, lon: 1)>
+    array([[[ 5.5]],
+
+        [[17.5]]])
+    Coordinates:
+    * year     (year) int64 2000 2001
+    Dimensions without coordinates: lat, lon
+    >>> geocat.comp.climatology(ts, 'season')
+    <xarray.DataArray (season: 4, lat: 1, lon: 1)>
+    array([[[10.]],
+
+        [[12.]],
+
+        [[ 9.]],
+
+        [[15.]]])
+    Coordinates:
+    * season   (season) object 'DJF' 'JJA' 'MAM' 'SON'
+    Dimensions without coordinates: lat, lon
     """
     data, time_invariant_vars, time_coord_name, time_dot_freq = _setup_clim_anom_input(
-        dset, freq, time_coord_name)
+        dset, freq, time_coord_name
+    )
 
     grouped = data.groupby(time_dot_freq)
     # TODO: Compute weighted climatologies when `time_bounds` are available
@@ -98,11 +139,9 @@ def climatology(
 
 
 def anomaly(
-        dset: typing.Union[xr.DataArray, xr.Dataset],
-        freq: str,
-        time_coord_name: str = None) -> typing.Union[xr.DataArray, xr.Dataset]:
-    """
-    Compute anomalies for a specified time frequency
+    dset: typing.Union[xr.DataArray, xr.Dataset], freq: str, time_coord_name: str = None
+) -> typing.Union[xr.DataArray, xr.Dataset]:
+    """Compute anomalies for a specified time frequency.
 
     Parameters
     ----------
@@ -124,9 +163,57 @@ def anomaly(
     -------
     computed_dset : xr.Dataset, xr.DataArray
        The computed anomaly data
+
+    Examples
+    --------
+    >>> import xarray as xr
+    >>> import pandas as pd
+    >>> import numpy as np
+    >>> import geocat.comp
+    >>> dates = pd.date_range(start="2000/01/01", freq="M", periods=24)
+    >>> ts = xr.DataArray(np.arange(24).reshape(24, 1, 1), dims=["time", "lat", "lon"], coords={"time": dates})
+    >>> ts
+    <xarray.DataArray (time: 24, lat: 1, lon: 1)>
+    array([[[ 0]],
+
+        [[ 1]],
+
+        [[ 2]],
+
+    ...
+
+        [[21]],
+
+        [[22]],
+
+        [[23]]])
+    Coordinates:
+    * time     (time) datetime64[ns] 2000-01-31 2000-02-29 ... 2001-12-31
+    Dimensions without coordinates: lat, lon
+    >>> geocat.comp.anomaly(ts, 'season')
+    <xarray.DataArray (time: 24, lat: 1, lon: 1)>
+    array([[[-10.]],
+
+        [[ -9.]],
+
+        [[ -7.]],
+
+    ...
+
+        [[  6.]],
+
+        [[  7.]],
+
+        [[ 13.]]])
+    Coordinates:
+    * time     (time) datetime64[ns] 2000-01-31 2000-02-29 ... 2001-12-31
+        season   (time) <U3 'DJF' 'DJF' 'MAM' 'MAM' ... 'SON' 'SON' 'SON' 'DJF'
+    Dimensions without coordinates: lat, lon
     """
+
     data, time_invariant_vars, time_coord_name, time_dot_freq = _setup_clim_anom_input(
-        dset, freq, time_coord_name)
+        dset, freq, time_coord_name
+    )
 
     clim = climatology(data, freq, time_coord_name)
     anom = data.groupby(time_dot_freq) - clim
