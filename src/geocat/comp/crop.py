cba60630f1ac2f845b48ec31966d54f7bb28ee62
anissa111 <amissaz@ucar.edu>
2021-03-03 16:29:31 -0700

    working? for array values

--- src/geocat/comp/crop.py
+++ src/geocat/comp/crop.py
@@ -33,3 +33,37 @@ def max_daylight(jday, lat):
 
     # convert inputs to numpy arrays for function call
     jday = np.asarray(jday)
+    lat = np.asarray(lat)
+
+    # get and store size dimensions
+    ntim = jday.shape
+    dimlat = lat.shape
+    nlat = dimlat[0]
+
+    # define constants
+    pi = np.pi
+    rad = pi / 180
+    pi2yr = 2 * pi / 365
+    latrad = lat * rad
+    con = 24 / pi
+
+    # check to ensure dimension of lat is not greater than two
+    if lat.ndim > 2:
+        raise ValueError('Number of dimensions of lat must be two or less')
+
+    # if  dimension of lat is 2, pull out size
+    if lat.ndim == 2:
+        mlon = dimlat[1]
+
+    # Equation 24 from fao56
+    sdec = 0.409 * np.sin(pi2yr * jday - 1.39)
+
+    ws = np.arccos(np.outer(-np.tan(latrad), np.tan(sdec)))
+    # ws1 = np.arccos(np.clip(np.outer(-np.tan(latrad), np.tan(sdec)), -0.99999, 0.99999))
+
+    dlm = ws * con
+    return dlm
+
+
+# max_daylight([15, 180, 246, 306], [-20, 0, 45])
+max_daylight(246, -20)
