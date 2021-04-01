5f42dfbc6d17911d13ff65245793460c89fe1009
michaelavs <michaela.sizemore@gmail.com>
2021-04-01 08:01:51 -0600

    Fixing formatting

--- test/test_skewt_params.py
+++ test/test_skewt_params.py
@@ -11,16 +11,15 @@ if "--cov" in str(sys.argv):
 else:
     from geocat.comp import get_skewt_vars, showalter_index
 
+p_upper = np.arange(1000, 200, -50) * units.hPa
+p_lower = np.arange(175, 0, -25) * units.hPa
+p = np.append(p_upper, p_lower)  # Pressure levels in hPa
+tc = np.linspace(30, -30, 23) * units.degC  # Env temp in degC
+tdc = np.linspace(10, -30, 23) * units.degC  # DewPt temp in degC
+pro = mpcalc.parcel_profile(p, tc[0], tdc[0])  # Parcel Profile
+
 
 def test_showalter_index():
-    p_upper = np.arange(1000, 200, -50) * units.hPa
-    p_lower = np.arange(175, 0, -25) * units.hPa
-    p = np.append(
-        p_upper,
-        p_lower,
-    )  # Pressure levels in hPa
-    tc = np.linspace(30, -30, 23) * units.degC  # Env temp in degC
-    tdc = np.linspace(10, -30, 23) * units.degC  # DewPt temp in degC
 
     result = showalter_index(p, tc, tdc)
     expected = 21.353962321924012 * units.delta_degree_Celsius
@@ -31,15 +30,6 @@ def test_showalter_index():
 
 
 def test_get_skewt_vars():
-    p_upper = np.arange(1000, 200, -50) * units.hPa
-    p_lower = np.arange(175, 0, -25) * units.hPa
-    p = np.append(
-        p_upper,
-        p_lower,
-    )  # Pressure levels in hPa
-    tc = np.linspace(30, -30, 23) * units.degC  # Env temp in degC
-    tdc = np.linspace(10, -30, 23) * units.degC  # DewPt temp in degC
-    pro = mpcalc.parcel_profile(p, tc[0], tdc[0])  # Parcel Profile
 
     result = get_skewt_vars(p, tc, tdc, pro)
     expected = 'Plcl= 747 Tlcl[C]= 6 Shox= 21 Pwat[cm]= 5 Cape[J]= 0'
