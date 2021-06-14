92d0c279f320cd37e88633d262733ff86377ae6a
anissa111 <anissaz@ucar.edu>
2021-06-14 13:51:23 -0600

    pull from old branch

--- test/test_relhum.py
+++ test/test_relhum.py
@@ -2,6 +2,7 @@ import sys
 import unittest
 
 import dask.array as da
+import dask
 import dask.distributed as dd
 import numpy as np
 import xarray as xr
@@ -16,32 +17,39 @@ else:
 
 
 class Test_relhum(unittest.TestCase):
-    p_def = [
-        100800, 100000, 95000, 90000, 85000, 80000, 75000, 70000, 65000, 60000,
-        55000, 50000, 45000, 40000, 35000, 30000, 25000, 20000, 17500, 15000,
-        12500, 10000, 8000, 7000, 6000, 5000, 4000, 3000, 2500, 2000
-    ]
 
-    t_def = [
-        302.45, 301.25, 296.65, 294.05, 291.55, 289.05, 286.25, 283.25, 279.85,
-        276.25, 272.65, 268.65, 264.15, 258.35, 251.65, 243.45, 233.15, 220.75,
-        213.95, 206.65, 199.05, 194.65, 197.15, 201.55, 206.45, 211.85, 216.85,
-        221.45, 222.45, 225.65
-    ]
+    @classmethod
+    def setUpClass(cls):
 
-    q_def = [
-        0.02038, 0.01903, 0.01614, 0.01371, 0.01156, 0.0098, 0.00833, 0.00675,
-        0.00606, 0.00507, 0.00388, 0.00329, 0.00239, 0.0017, 0.001, 0.0006,
-        0.0002, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-    ]
+        cls.p_def = [
+            100800, 100000, 95000, 90000, 85000, 80000, 75000, 70000, 65000,
+            60000, 55000, 50000, 45000, 40000, 35000, 30000, 25000, 20000,
+            17500, 15000, 12500, 10000, 8000, 7000, 6000, 5000, 4000, 3000,
+            2500, 2000
+        ]
 
-    rh_gt_1 = 46.4
+        cls.t_def = [
+            302.45, 301.25, 296.65, 294.05, 291.55, 289.05, 286.25, 283.25,
+            279.85, 276.25, 272.65, 268.65, 264.15, 258.35, 251.65, 243.45,
+            233.15, 220.75, 213.95, 206.65, 199.05, 194.65, 197.15, 201.55,
+            206.45, 211.85, 216.85, 221.45, 222.45, 225.65
+        ]
 
-    rh_gt_2 = [
-        79.8228, 79.3578, 84.1962, 79.4898, 73.989, 69.2401, 66.1896, 61.1084,
-        64.21, 63.8305, 58.0412, 60.8194, 57.927, 62.3734, 62.9706, 73.8184,
-        62.71, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-    ]
+        cls.q_def = [
+            0.02038, 0.01903, 0.01614, 0.01371, 0.01156, 0.0098, 0.00833,
+            0.00675, 0.00606, 0.00507, 0.00388, 0.00329, 0.00239, 0.0017, 0.001,
+            0.0006, 0.0002, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        ]
+
+        cls.rh_gt_1 = 46.4
+
+        cls.rh_gt_2 = [
+            79.8228, 79.3578, 84.1962, 79.4898, 73.989, 69.2401, 66.1896,
+            61.1084, 64.21, 63.8305, 58.0412, 60.8194, 57.927, 62.3734, 62.9706,
+            73.8184, 62.71, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        ]
+
+        cls.client = dd.Client()
 
     def test_float_input(self):
         p = 1000. * 100
@@ -63,44 +71,27 @@ class Test_relhum(unittest.TestCase):
 
         assert np.allclose(relhum(t, q, p), self.rh_gt_2, atol=0.1)
 
-    def test_xarray_input(self):
-        p = xr.DataArray(self.p_def)
-        t = xr.DataArray(self.t_def)
-        q = xr.DataArray(self.q_def)
+    def test_dims_error(self):
+        self.assertRaises(ValueError, relhum, self.t_def[:10], self.q_def[:10],
+                          self.p_def[:9])
+
+    def test_xarray_type_error(self):
+        self.assertRaises(TypeError, relhum, self.t_def,
+                          xr.DataArray(self.q_def), self.p_def)
+
+    def test_dask_compute(self):
+        p = xr.DataArray(self.p_def).chunk(10)
+        t = xr.DataArray(self.t_def).chunk(10)
+        q = xr.DataArray(self.q_def).chunk(10)
 
         assert np.allclose(relhum(t, q, p), self.rh_gt_2, atol=0.1)
 
-    def test_dask_unchunked_input(self):
-        p = da.from_array(self.p_def)
-        t = da.from_array(self.t_def)
-        q = da.from_array(self.q_def)
+    def test_dask_lazy(self):
+        p = xr.DataArray(self.p_def).chunk(10)
+        t = xr.DataArray(self.t_def).chunk(10)
+        q = xr.DataArray(self.q_def).chunk(10)
 
-        # Start dask cluster
-        cluster = dd.LocalCluster(n_workers=3, threads_per_worker=2)
-        print(cluster.dashboard_link)
-        client = dd.Client(cluster)
-
-        out = map_blocks(relhum, t, q, p).compute()
-
-        assert np.allclose(out, self.rh_gt_2, atol=0.1)
-
-        client.shutdown()
-
-    def test_dask_chunked_input(self):
-        p = da.from_array(self.p_def, chunks="auto")
-        t = da.from_array(self.t_def, chunks="auto")
-        q = da.from_array(self.q_def, chunks="auto")
-
-        # Start dask cluster
-        cluster = dd.LocalCluster(n_workers=3, threads_per_worker=2)
-        print(cluster.dashboard_link)
-        client = dd.Client(cluster)
-
-        out = client.submit(relhum, t, q, p).result()
-
-        assert np.allclose(out, self.rh_gt_2, atol=0.1)
-
-        client.shutdown()
+        assert isinstance(relhum(t, q, p).data, dask.array.Array)
 
 
 class Test_relhum_water(unittest.TestCase):
