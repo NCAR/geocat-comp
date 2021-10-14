bc9436f80c1d99dd4c731ff8e073a0e2593016d0
erogluorhan <erogluorhan@gmail.com>
2021-10-14 16:24:23 -0600

    pre-commit fixes

--- src/geocat/comp/interpolation.py
+++ src/geocat/comp/interpolation.py
@@ -10,10 +10,42 @@ __pres_lev_mandatory__ = np.array([
 __pres_lev_mandatory__ = __pres_lev_mandatory__ * 100.0  # Convert mb to Pa
 
 
+def _func_interpolate(method='linear'):
+    """Define interpolation function."""
+
+    if method == 'linear':
+        func_interpolate = metpy.interpolate.interpolate_1d
+    elif method == 'log':
+        func_interpolate = metpy.interpolate.log_interpolate_1d
+    else:
+        raise ValueError(f'Unknown interpolation method: {method}. '
+                         f'Supported methods are: "log" and "linear".')
+
+    return func_interpolate
+
+
 def _pressure_from_hybrid(psfc, hya, hyb, p0=100000.):
     """Calculate pressure at the hybrid levels."""
+
+    # p(k) = hya(k) * p0 + hyb(k) * psfc
+
     # This will be in Pa
     return hya * p0 + hyb * psfc
+
+
+def _sigma_from_hybrid(psfc, hya, hyb, p0=100000.):
+    """Calculate sigma at the hybrid levels."""
+
+    # sig(k) = hya(k) * p0 / psfc + hyb(k)
+
+    # This will be in Pa
+    return hya * p0 / psfc + hyb
+
+
+def _vertical_remap(func_interpolate, new_levels, xcoords, data, interp_axis=0):
+    """Execute the defined interpolation function on data."""
+
+    return func_interpolate(new_levels, xcoords, data, axis=interp_axis)
 
 
 def interp_hybrid_to_pressure(data: xr.DataArray,
@@ -24,7 +56,9 @@ def interp_hybrid_to_pressure(data: xr.DataArray,
                               new_levels: np.ndarray = __pres_lev_mandatory__,
                               lev_dim: str = None,
                               method: str = 'linear') -> xr.DataArray:
-    """Interpolate data from hybrid-sigma levels to isobaric levels.
+    """Interpolate data from hybrid-sigma levels to isobaric levels. Keeps
+    attributes (i.e. meta information) of the input data in the output as
+    default.
 
     Notes
     -----
@@ -73,6 +107,11 @@ def interp_hybrid_to_pressure(data: xr.DataArray,
                 "Unable to determine vertical dimension name. Please specify the name via `lev_dim` argument.'"
             )
 
+    try:
+        func_interpolate = _func_interpolate(method)
+    except ValueError as vexc:
+        raise ValueError(vexc.args[0])
+
     interp_axis = data.dims.index(lev_dim)
 
     # Calculate pressure levels at the hybrid levels
@@ -80,20 +119,6 @@ def interp_hybrid_to_pressure(data: xr.DataArray,
 
     # Make pressure shape same as data shape
     pressure = pressure.transpose(*data.dims)
-
-    # Define interpolation function
-    if method == 'linear':
-        func_interpolate = metpy.interpolate.interpolate_1d
-    elif method == 'log':
-        func_interpolate = metpy.interpolate.log_interpolate_1d
-    else:
-        raise ValueError(f'Unknown interpolation method: {method}. '
-                         f'Supported methods are: "log" and "linear".')
-
-    def _vertical_remap(data, pressure):
-        """Define interpolation function."""
-
-        return func_interpolate(new_levels, pressure, data, axis=interp_axis)
 
     ###############################################################################
     # Workaround
@@ -144,8 +169,11 @@ def interp_hybrid_to_pressure(data: xr.DataArray,
     from dask.array.core import map_blocks
     output = map_blocks(
         _vertical_remap,
-        data.data,
+        func_interpolate,
+        new_levels,
         pressure.data,
+        data.data,
+        interp_axis,
         chunks=out_chunks,
         dtype=data.dtype,
         drop_axis=[interp_axis],
@@ -155,7 +183,7 @@ def interp_hybrid_to_pressure(data: xr.DataArray,
     # End of Workaround
     ###############################################################################
 
-    output = xr.DataArray(output)
+    output = xr.DataArray(output, name=data.name, attrs=data.attrs)
 
     # Set output dims and coords
     dims = [
@@ -174,5 +202,98 @@ def interp_hybrid_to_pressure(data: xr.DataArray,
             coords.update({"plev": new_levels})
 
     output = output.transpose(*dims).assign_coords(coords)
+
+    return output
+
+
+def interp_sigma_to_hybrid(data: xr.DataArray,
+                           sig_coords: xr.DataArray,
+                           ps: xr.DataArray,
+                           hyam: xr.DataArray,
+                           hybm: xr.DataArray,
+                           p0: float = 100000.,
+                           lev_dim: str = None,
+                           method: str = 'linear') -> xr.DataArray:
+    """Interpolate data from sigma to hybrid coordinates.  Keeps attributes
+    (i.e. meta information) of the input data in the output as default.
+
+    Parameters
+    ----------
+    data : :class:`xarray.DataArray`
+        Multidimensional data array, which holds sigma levels and has a `lev_dim` coordinate.
+
+    sig_coords : :class:`xarray.DataArray`
+        A one-dimensional array of sigma coordinates of `lev_dim` of `data`.
+
+    ps : :class:`xarray.DataArray`
+        A multi-dimensional array of surface pressures (Pa), same time/space shape as data.
+
+    hyam, hybm : :class:`xarray.DataArray`
+        One-dimensional arrays containing the hybrid A and B coefficients. Must have the same
+        dimension as the output hybrid levels.
+
+    p0 : :class:`float`, Optional
+        Scalar numeric value equal to surface reference pressure (Pa). Defaults to 100000 Pa.
+
+    lev_dim : :class:`str`, Optional
+        String that is the name of level dimension in data. Defaults to "lev".
+
+    method : :class:`str`, Optional
+        String that is the interpolation method; can be either "linear" or "log". Defaults to "linear".
+
+    Returns
+    -------
+    output : :class:`xarray.DataArray`
+        Interpolated data with hybrid levels
+
+    See Also
+    --------
+    Related NCL Functions:
+    `sigma2hybrid <https://www.ncl.ucar.edu/Document/Functions/Built-in/sigma2hybrid.shtml>`_
+    """
+
+    # Determine the level dimension and then the interpolation axis
+    if lev_dim is None:
+        try:
+            lev_dim = data.cf["vertical"].name
+        except Exception:
+            raise ValueError(
+                "Unable to determine vertical dimension name. Please specify the name via `lev_dim` argument.'"
+            )
+
+    try:
+        func_interpolate = _func_interpolate(method)
+    except ValueError as vexc:
+        raise ValueError(vexc.args[0])
+
+    # Calculate sigma levels at the hybrid levels
+    sigma = _sigma_from_hybrid(ps, hyam, hybm, p0)  # Pa
+
+    non_lev_dims = list(data.dims)
+    if (data.ndim > 1):
+        non_lev_dims.remove(lev_dim)
+        data_stacked = data.stack(combined=non_lev_dims).transpose()
+        sigma_stacked = sigma.stack(combined=non_lev_dims).transpose()
+
+        h_coords = sigma_stacked[0, :].copy()
+
+        output = data_stacked[:, :len(hyam)].copy()
+
+        for idx, (d, s) in enumerate(zip(data_stacked, sigma_stacked)):
+            output[idx, :] = xr.DataArray(
+                _vertical_remap(func_interpolate, s, sig_coords, d))
+
+        # Make output shape same as data shape
+        output = output.unstack().transpose(*data.dims)
+    else:
+        h_coords = sigma
+
+        output = data[:len(hyam)].copy()
+        output[:len(hyam)] = xr.DataArray(
+            _vertical_remap(func_interpolate, sigma, sig_coords, data))
+
+    # Set output dims and coords
+    output = output.rename({lev_dim: 'hlev'})
+    output = output.assign_coords({"hlev": h_coords.data})
 
     return output
