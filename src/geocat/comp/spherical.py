ab40e1c2621a2b313d8fdb5b6b7cb3c079e4e14c
pilotchute <akootz@ucar.edu>
2021-10-28 13:10:08 -0600

    lunch break
    Signed-off-by: pilotchute <akootz@ucar.edu>

--- src/geocat/comp/spherical.py
+++ src/geocat/comp/spherical.py
@@ -14,7 +14,7 @@ def harmonic_decomposition(
     input_phi: InputData,
     harms: Harms = None,
     max_harm: int = None,
-):
+) -> list:
     results = []
 
     # if no harmonic info provided by the user:
@@ -32,6 +32,8 @@ def harmonic_decomposition(
         results.append(
             np.sum(np.multiply(input_data, ss.sph_harm(m, n, theta, phi)),
                    axis=(0, 1)))
+
+    return results
 
 
 # ''' todo
