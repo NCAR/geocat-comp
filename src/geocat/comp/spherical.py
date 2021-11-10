11dc9784a9d76dd4535a59742df8ac9f6267b233
pilotchute <akootz@ucar.edu>
2021-11-10 16:20:07 -0700

    wednesday my dudettes
    Signed-off-by: pilotchute <akootz@ucar.edu>

--- src/geocat/comp/spherical.py
+++ src/geocat/comp/spherical.py
@@ -46,10 +46,32 @@ def harmonic_decomposition(
     return results
 
 
-# ''' todo
-# def harmonic_recomposition(
-#         input_data: ValidData,
-#         input_theta: ValidData,
-#         input_phi: ValidData,
-#         harms: Harms,
-# ):
+def harmonic_recomposition(
+    input_data: ValidData,
+    input_theta: ValidData,
+    input_phi: ValidData,
+    harms: Harms = None,
+    max_harm: int = None,
+):
+    # if no harmonic info provided by the user:
+    if max_harm is None and harms is None:
+        max_harm = 24  # 300 total harmonics
+
+    # in the case of max_harm, provide full set up to max_harm
+    if harms is None and max_harm is not None:
+        harms = []
+        for n in range(max_harm):
+            for m in range(n + 1):
+                harms.append([m, n])
+
+    if type(input_theta) is xr.DataArray:
+        results = xr.DataArray(np.zeros(input_theta.shape, dtype=complex),
+                               dims=input_theta.dims).chunk(input_theta.chunks)
+    else:
+        results = np.zeros(input_theta.shape, dtype=complex)
+
+    for harm, value in zip(harms, input_data):
+        sphere = ss.sph_harm(harm[0], harm[1], input_theta, input_phi)
+        results += sphere.real * value.real + sphere.imag * value.imag * 1j
+
+    return results
