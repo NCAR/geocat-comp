5c8917b24eb0f7e95ebaeb5adf1801bf13f83b70
pilotchute <akootz@ucar.edu>
2021-11-10 15:02:17 -0700

    wednesday my dudettes
    Signed-off-by: pilotchute <akootz@ucar.edu>

--- src/geocat/comp/spherical.py
+++ src/geocat/comp/spherical.py
@@ -23,18 +23,26 @@ def harmonic_decomposition(
     # in the case of max_harm, provide full set up to max_harm
     if harms is None and max_harm is not None:
         harms = []
-        for n in range(max_harm):
+        for n in range(max_harm + 1):
             for m in range(n + 1):
                 harms.append([m, n])
 
     results = []
-    input_data_scaled = np.multiply(demo_data, scale_phi).persist()
+    scale0 = 1 / (np.sum(scale_phi, axis=(0, 1)) * ss.sph_harm(0, 0, 1, 1)**2)
+    scale0 = scale0.compute().persist()
+    scale1 = scale0 * 2
+    # print(scale1.compute())
+    input_data_scaled = np.multiply(demo_data, input_scale).persist()
     for harm in harms:
         results.append(
             np.sum(np.multiply(
                 input_data_scaled,
                 ss.sph_harm(harm[0], harm[1], input_theta, input_phi)),
                    axis=(0, 1)))
+        if harm[0] == 0:
+            results[-1] = results[-1] * scale0
+        else:
+            results[-1] = results[-1] * scale1
 
     return results
 
