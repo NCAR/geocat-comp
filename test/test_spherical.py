7931165376d9c4a49c47d15d467944d8b092d8a8
pilotchute <akootz@ucar.edu>
2022-03-15 15:13:44 -0600

    test cases updated to unittest.TestCase object
    Signed-off-by: pilotchute <akootz@ucar.edu>

--- test/test_spherical.py
+++ test/test_spherical.py
@@ -9,77 +9,95 @@ from typing import Union
 # packages otherwise
 if "--cov" in str(sys.argv):
     from src.geocat.comp import decomposition, recomposition, scale_voronoi
+elif "-v" in str(sys.argv):
+    from src.geocat.comp import decomposition, recomposition, scale_voronoi
 else:
     from geocat.comp import decomposition, recomposition, scale_voronoi
 
-max_harm = 23
-num_phi = 360
-num_theta = 720
 
-theta = np.linspace(0, ma.tau - ma.tau / num_theta, num_theta)
-phi = np.linspace(ma.pi / (2 * num_phi), ma.pi - ma.pi / (2 * num_phi), num_phi)
-theta_np, phi_np = np.meshgrid(theta, phi)
-theta_xr = xr.DataArray(theta_np, dims=['lat', 'lon'])
-phi_xr = xr.DataArray(phi_np, dims=['lat', 'lon'])
-test_scale_np = np.sin(phi_np)
-test_scale_xr = xr.DataArray(test_scale_np, dims=['lat', 'lon'])
+class Test_Spherical(unittest.TestCase):
 
-test_data = np.zeros(theta_np.shape)
-test_results = []
-test_harmonics = []
-for n in range(max_harm + 1):
-    for m in range(n + 1):
-        test_harmonics.append([m, n])
-        test_results.append(0)
-        if n in [0, 2, 3, 5, 7, 11, 13, 17, 19, 23
-                ] and m in [0, 2, 3, 5, 7, 11, 13, 17, 19, 23]:
-            if m in [2, 5, 11, 17, 23]:
-                test_data += ss.sph_harm(m, n, theta_np, phi_np).imag
-                test_results[-1] = 1j
-            else:
-                test_data += ss.sph_harm(m, n, theta_np, phi_np).real
-                test_results[-1] = 1
+    @classmethod
+    def setUpClass(cls):
+        max_harm = 23
+        num_phi = 360
+        num_theta = 720
 
-test_harmonics_np = np.array(test_harmonics)
-test_harmonics_xr = xr.DataArray(test_harmonics_np, dims=['har',
-                                                          'm,n']).compute()
-test_data_np = test_data
-test_data_xr = xr.DataArray(test_data_np, dims=['lat', 'lon']).compute()
-test_results_np = np.array(test_results)
-test_results_xr = xr.DataArray(test_results_np, dims=['har']).compute()
+        theta = np.linspace(0, ma.tau - ma.tau / num_theta, num_theta)
+        phi = np.linspace(ma.pi / (2 * num_phi), ma.pi - ma.pi / (2 * num_phi),
+                          num_phi)
+        cls.theta_np, cls.phi_np = np.meshgrid(theta, phi)
+        cls.theta_xr = xr.DataArray(cls.theta_np, dims=['lat', 'lon'])
+        cls.phi_xr = xr.DataArray(cls.phi_np, dims=['lat', 'lon'])
+        cls.test_scale_np = np.sin(phi_np)
+        cls.test_scale_xr = xr.DataArray(cls.test_scale_np,
+                                         dims=['lat', 'lon']).compute()
 
+        test_data = np.zeros(cls.theta_np.shape)
+        test_results = []
+        test_harmonics = []
+        for n in range(max_harm + 1):
+            for m in range(n + 1):
+                test_harmonics.append([m, n])
+                test_results.append(0)
+                if n in [0, 2, 3, 5, 7, 11, 13, 17, 19, 23
+                        ] and m in [0, 2, 3, 5, 7, 11, 13, 17, 19, 23]:
+                    if m in [2, 5, 11, 17, 23]:
+                        test_data += ss.sph_harm(m, n, theta_np, phi_np).imag
+                        test_results[-1] = 1j
+                    else:
+                        test_data += ss.sph_harm(m, n, theta_np, phi_np).real
+                        test_results[-1] = 1
 
-def test_decomposition_np():
-    results_np = decomposition(test_data_np, test_scale_np, theta_np, phi_np)
-    np.testing.assert_almost_equal(results_np, test_results_np, decimal=3)
+        cls.test_harmonics_np = np.array(test_harmonics)
+        cls.test_harmonics_xr = xr.DataArray(
+            cls.test_harmonics_np,
+            dims=['har', 'm,n'],
+        ).compute()
+        cls.test_data_np = test_data
+        cls.test_data_xr = xr.DataArray(
+            cls.test_data_np,
+            dims=['lat', 'lon'],
+        ).compute()
+        cls.test_results_np = np.array(test_results)
+        cls.test_results_xr = xr.DataArray(
+            cls.test_results_np,
+            dims=['har'],
+        ).compute()
 
+    def test_decomposition_np(self):
+        results_np = decomposition(self.test_data_np, self.test_scale_np,
+                                   self.theta_np, self.phi_np)
+        np.testing.assert_almost_equal(results_np,
+                                       self.test_results_np,
+                                       decimal=3)
 
-def test_decomposition_xr():
-    results_xr = decomposition(test_data_xr, test_scale_xr, theta_xr, phi_xr)
-    np.testing.assert_almost_equal(results_xr.to_numpy(),
-                                   test_results_xr.to_numpy(),
-                                   decimal=3)
+    def test_decomposition_xr(self):
+        results_xr = decomposition(self.test_data_xr, self.test_scale_xr,
+                                   self.theta_xr, self.phi_xr)
+        np.testing.assert_almost_equal(results_xr.to_numpy(),
+                                       self.test_results_xr.to_numpy(),
+                                       decimal=3)
 
+    def test_recomposition_np(self):
+        data_np = recomposition(self.test_results_np, self.theta_np,
+                                self.phi_np)
+        np.testing.assert_almost_equal(data_np, self.test_data_np)
 
-def test_recomposition_np():
-    data_np = recomposition(test_results_np, theta_np, phi_np)
-    np.testing.assert_almost_equal(data_np, test_data_np)
+    def test_recomposition_xr(self):
+        data_xr = recomposition(self.test_results_xr, self.theta_xr,
+                                self.phi_xr)
+        np.testing.assert_almost_equal(data_xr.to_numpy(),
+                                       self.test_data_xr.to_numpy())
 
+    def test_scale_voronoi_np(self):
+        scale_np = scale_voronoi(self.theta_np, self.phi_np)
+        np.testing.assert_almost_equal(
+            scale_np / np.sum(scale_np, axis=(0, 1)),
+            self.test_scale_np / np.sum(self.test_scale_np, axis=(0, 1)))
 
-def test_recomposition_xr():
-    data_xr = recomposition(test_results_xr, theta_xr, phi_xr)
-    np.testing.assert_almost_equal(data_xr.to_numpy(), test_data_xr.to_numpy())
-
-
-def test_scale_voronoi_np():
-    scale_np = scale_voronoi(theta_np, phi_np)
-    np.testing.assert_almost_equal(
-        scale_np / np.sum(scale_np, axis=(0, 1)),
-        test_scale_np / np.sum(test_scale_np, axis=(0, 1)))
-
-
-def test_scale_voronoi_xr():
-    scale_xr = scale_voronoi(theta_xr, phi_xr)
-    np.testing.assert_almost_equal(
-        scale_xr / np.sum(scale_xr, axis=(0, 1)),
-        test_scale_xr / np.sum(test_scale_xr, axis=(0, 1)))
+    def test_scale_voronoi_xr(self):
+        scale_xr = scale_voronoi(self.theta_xr, self.phi_xr)
+        np.testing.assert_almost_equal(
+            scale_xr / np.sum(scale_xr, axis=(0, 1)),
+            self.test_scale_xr / np.sum(self.test_scale_xr, axis=(0, 1)))
