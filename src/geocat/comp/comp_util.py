f9e9297883a4cf07fc190ce9b62469d2ec503175
haniyeka <haniye.kashgarani@gmail.com>
2022-07-08 12:53:49 -0600

    ported relhum to GPU

--- src/geocat/comp/comp_util.py
+++ src/geocat/comp/comp_util.py
@@ -8,3 +8,12 @@ def _is_duck_array(value):
     return (hasattr(value, "ndim") and hasattr(value, "shape") and
             hasattr(value, "dtype") and hasattr(value, "__array_function__") and
             hasattr(value, "__array_ufunc__"))
+
+def _import_cupy():
+    """imports the cupy and checks if not installed"""
+    try:
+        import cupy as cp
+        return cp
+    except ImportError as e:
+        print(f"Cupy is not installed for GPU computation!")
+        pass  # module doesn't exist, deal with it. 
