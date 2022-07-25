2b87e61332864dc7a7ab53f22b168ea43be18b95
Heather Craker <hcraker@ucar.edu>
2022-07-25 18:17:33 -0400

    Added simple test for pearson_r

--- test/test_correlation.py
+++ test/test_correlation.py
@@ -10,31 +10,33 @@ if "--cov" in str(sys.argv):
 else:
     from geocat.comp import pearson_r
 
-def _get_dummy_data():
-    """
-    Returns a simple xarray dataset to test with.
-    """
-    # Coordinates
-    time = xr.cftime_range(start='2022-07-25',
-                           end='2022-07-31',
-                           freq='D')
-    lats = np.linspace(start=-90, stop=90, num=2, dtype='float32')
-    lons = np.linspace(start=-180, stop=180, num=18, dtype='float32')
-
-    # Create data variable
-    np.random.seed(0)
-    a = np.random.random_sample((len(time), len(lats), len(lons)))
-    b = np.power(a, 2)
-    ds = xr.Dataset(data_vars={'a': (('time', 'lat', 'lon'), a),
-                               'b': (('time', 'lat', 'lon'), b)},
-                    coords={
-                        'time': time,
-                        'lat': lats,
-                        'lon': lons
-                    },
-                    attrs={'description': 'Test data'})
-    return ds
-
 class Test_pearson_r(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Coordinates
+        time = xr.cftime_range(start='2022-07-25',
+                               end='2022-07-26',
+                               freq='D')
+        lats = np.linspace(start=-90, stop=90, num=2, dtype='float32')
+        lons = np.linspace(start=-180, stop=180, num=18, dtype='float32')
+
+        # Create data variables
+        np.random.seed(0)
+        a = np.random.random_sample((len(lats), len(lons)))
+        b = np.power(a, 2)
+
+        cls.ds = xr.Dataset(data_vars={'a': (('lat', 'lon'), a),
+                                   'b': (('lat', 'lon'), b)},
+                        coords={
+                            'lat': lats,
+                            'lon': lons
+                        },
+                        attrs={'description': 'Test data'})
+        cls.unweighted_r = 0.960868163
+
     def test_pearson_r(self):
-        assert False
+        a = self.ds.a
+        b = self.ds.b
+        result = pearson_r(a, b)
+        print(self.unweighted_r, result)
+        assert np.allclose(self.unweighted_r, result)
