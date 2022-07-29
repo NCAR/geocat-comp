3c36c6299568e47af53f95f77652366e81185f15
Heather Craker <hcraker@ucar.edu>
2022-07-29 10:56:55 -0400

    Adjust warning messages

--- src/geocat/comp/correlation.py
+++ src/geocat/comp/correlation.py
@@ -5,17 +5,23 @@ import xarray as xr
 import warnings
 
 def pearson_r(a, b, dim=None, weights=None, skipna=False, keep_attrs=False, axis=None):
-    if not isinstance(a, xr.DataArray) and not isinstance(b, xr.DataArray):
+    if not isinstance(a, xr.DataArray) and not isinstance(b, xr.DataArray):  # if a and v are not xr.DataArrays
         if (dim is not None) and (axis is not None):
-            warnings.warn("The `axis` keyword is unused with xarray.DataArray inputs")
-        if axis is None: # squash array to 1D for element wise computation
+            warnings.warn("The `dim` keyword is unused with non xarray.DataArray inputs")
+        if axis is None:  # squash array to 1D for element wise computation
             axis = 0
             a = np.ravel(a)
             b = np.ravel(b)
             if weights is not None:
                 weights = np.ravel(weights)
-        return xs_internal._pearson_r(a, b, weights, axis, skipna)
-    else:
+        try:
+            return xs_internal._pearson_r(a, b, weights, axis, skipna)
+        except ValueError:
+            print('Data along `axis` must have the same dimension as `weights`')
+    else:  # if a and b are xr.DataArrays
         if (dim is not None) and (axis is not None):
-            warnings.warn("The `dim` keyword is unused with non xarray.DataArray inputs")
-        return xs.pearson_r(a, b, dim, weights, skipna, keep_attrs)
+            warnings.warn("The `axis` keyword is unused with xarray.DataArray inputs")
+        try:
+            return xs.pearson_r(a, b, dim, weights, skipna, keep_attrs)
+        except ValueError:
+            print('Data along `dim` must have the same dimension as `weights`')
