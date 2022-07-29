2bdee757e06fe51c088fd0e597b5e4cc6f1c06ed
Heather Craker <hcraker@ucar.edu>
2022-07-29 17:46:08 -0400

    Use 3x4x5 size test data

--- test/test_correlation.py
+++ test/test_correlation.py
@@ -14,17 +14,17 @@ class Test_pearson_r(TestCase):
     @classmethod
     def setUpClass(cls):
         # Coordinates
-        times = xr.cftime_range(start='2022-07-25',
-                               end='2022-07-26',
+        times = xr.cftime_range(start='2022-08-01',
+                               end='2022-08-05',
                                freq='D')
-        lats = np.linspace(start=-90, stop=90, num=2, dtype='float32')
-        lons = np.linspace(start=-180, stop=180, num=18, dtype='float32')
+        lats = np.linspace(start=-90, stop=90, num=3, dtype='float32')
+        lons = np.linspace(start=-180, stop=180, num=4, dtype='float32')
 
         # Create data variables
         np.random.seed(0)
         cls.a = np.random.random_sample((len(lats), len(lons), len(times)))
         cls.b = np.power(cls.a, 2)
-        cls.weights = np.arange(1, 73).reshape(2, 18, 2)
+        cls.weights = np.arange(1, 61).reshape(3, 4, 5)
         cls.ds = xr.Dataset(data_vars={'a': (('lat', 'lon', 'time'), cls.a),
                                        'b': (('lat', 'lon', 'time'), cls.b),
                                        'weights': (('lat', 'lon', 'time'), cls.weights)},
@@ -35,10 +35,11 @@ class Test_pearson_r(TestCase):
                         },
                         attrs={'description': 'Test data'})
 
-        cls.unweighted_r = 0.966220619
-        cls.weighted_r = 0.96862701
-        cls.weighted_r_lon = [[0.952141203, 0.970682807],
-                               [0.981126127, 0.968390927]]
+        cls.unweighted_r = 0.963472086
+        cls.weighted_r = 0.964263374
+        cls.weighted_r_time = [[0.995914941, 0.996109426, 0.964059793, 0.996451319],
+                               [0.976498563, 0.968421665, 0.955529222, 0.999373275],
+                               [0.969600314, 0.979661379, 0.97071373, 0.988741167]]
 
     # Testing numpy inputs
     def test_pearson_r_np(self):
@@ -59,6 +60,13 @@ class Test_pearson_r(TestCase):
         b = self.b
         self.assertWarns(Warning, pearson_r, a, b, dim='lat', axis=0)
 
+    def test_pearson_r_np_time(self):
+        a = self.a
+        b = self.b
+        w = self.weights
+        result = pearson_r(a, b, weights=w, axis=2)
+        assert np.allclose(self.weighted_r_time, result)
+
     # Testing xarray inputs
     def test_pearson_r_xr(self):
         a = self.ds.a
@@ -78,11 +86,9 @@ class Test_pearson_r(TestCase):
         b = self.ds.b
         self.assertWarns(Warning, pearson_r, a, b, dim='lat', axis=0)
 
-    def test_pearson_r_xr_lon(self):
+    def test_pearson_r_xr_time(self):
         a = self.ds.a
         b = self.ds.b
-        w = self.ds.weights[0,:,0]
-        result = pearson_r(a, b, weights=w, dim='lon')
-        print(result)
-        print(self.weighted_r_lon)
-        assert np.allclose(self.weighted_r_lon, result)
+        w = self.ds.weights[0,0,:]  # size of dim and weights must be the same
+        result = pearson_r(a, b, weights=w, dim='time')
+        assert np.allclose(self.weighted_r_time, result)
