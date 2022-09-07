6f18f33b5eadf5c188192f097f6f2e33c5ef8a2a
anissa111 <anissaz@ucar.edu>
2022-09-07 13:09:56 -0600

    try not importing through comp

--- docs/conf.py
+++ docs/conf.py
@@ -36,9 +36,6 @@ class Mock(MagicMock):
 MOCK_MODULES = ["xarray", "dask", "dask.array", "dask.array.core"]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
-# different mock module methods
-autodoc_mock_imports = ["geocat.f2py"]
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
