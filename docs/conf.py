abf7a302abcdcdc49e003422a5768c8f20892ce6
anissa111 <anissaz@ucar.edu>
2022-09-07 12:50:40 -0600

    try mocking

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,9 @@ class Mock(MagicMock):
         return MagicMock()
 
 
-MOCK_MODULES = ["xarray", "dask", "dask.array", "dask.array.core"]
+MOCK_MODULES = [
+    "xarray", "dask", "dask.array", "dask.array.core", "geocat.f2py"
+]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
