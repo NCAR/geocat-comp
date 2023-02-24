90588f1a5bafb02373a52f279e02256f3295d66b
anissa111 <anissaz@ucar.edu>
2023-02-24 15:16:35 -0700

    add helper function to generate wrapper docstrings

--- src/geocat/comp/__init__.py
+++ src/geocat/comp/__init__.py
@@ -14,10 +14,16 @@ from .meteorology import (dewtemp, heat_index, showalter_index, relhum,
                           relhum_ice, relhum_water,
                           actual_saturation_vapor_pressure, max_daylight,
                           psychrometric_constant, saturation_vapor_pressure,
-                          saturation_vapor_pressure_slope, delta_pressure)
+                          saturation_vapor_pressure_slope, delta_pressure, dpres_plev)
 from .skewt_params import get_skewt_vars, showalter_index
 from .spherical import decomposition, recomposition, scale_voronoi
 from .stats import eofunc, eofunc_eofs, eofunc_pcs, eofunc_ts, pearson_r
+
+from .util import _generate_wrapper_docstring
+
+# generate wrapper docstrings
+_generate_wrapper_docstring(dpres_plev, delta_pressure)
+
 # bring all functions from geocat.f2py into the geocat.comp namespace
 try:
     from geocat.f2py import *
