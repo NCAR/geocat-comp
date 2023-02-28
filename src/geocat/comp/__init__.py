29430a394cfc782aa6c21f3765cd5d7f8b71b7e4
Heather Craker <hcraker@ucar.edu>
2023-02-28 16:56:11 -0500

    pre-commit

--- src/geocat/comp/__init__.py
+++ src/geocat/comp/__init__.py
@@ -5,8 +5,7 @@ from .fourier_filters import (fourier_band_block, fourier_band_pass,
                               fourier_low_pass)
 from .gradient import gradient, _arc_lon_wgs84, _arc_lat_wgs84, _rad_lat_wgs84
 from .interpolation import interp_hybrid_to_pressure, interp_sigma_to_hybrid, interp_multidim
-from .meteorology import (dewtemp, heat_index, relhum,
-                          relhum_ice, relhum_water,
+from .meteorology import (dewtemp, heat_index, relhum, relhum_ice, relhum_water,
                           actual_saturation_vapor_pressure, max_daylight,
                           psychrometric_constant, saturation_vapor_pressure,
                           saturation_vapor_pressure_slope)
