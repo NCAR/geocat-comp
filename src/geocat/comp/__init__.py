44aa8ad7479ccc104f3a7ef2958e852186d3cbdc
Julia Kent <46687291+jukent@users.noreply.github.com>
2023-03-01 13:56:25 -0700

    Update src/geocat/comp/__init__.py
    Co-authored-by: Heather Craker <hcraker@ucar.edu>

--- src/geocat/comp/__init__.py
+++ src/geocat/comp/__init__.py
@@ -9,7 +9,6 @@ from .meteorology import (dewtemp, heat_index, relhum, relhum_ice, relhum_water,
                           actual_saturation_vapor_pressure, max_daylight,
                           psychrometric_constant, saturation_vapor_pressure,
                           saturation_vapor_pressure_slope, delta_pressure)
-from .skewt_params import get_skewt_vars, showalter_index
 from .spherical import decomposition, recomposition, scale_voronoi
 from .stats import eofunc, eofunc_eofs, eofunc_pcs, eofunc_ts, pearson_r
 # bring all functions from geocat.f2py into the geocat.comp namespace
