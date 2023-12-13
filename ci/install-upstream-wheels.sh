a88c854e2b9d4d3d6b50342b1fd2773a6897a4f4
anissa111 <anissaz@ucar.edu>
2023-12-13 11:10:15 -0700

    remove old upstream dev env yml, add script

---
+++ ci/install-upstream-wheels.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# adapted from https://github.com/pydata/xarray/blob/main/ci/install-upstream-wheels.sh
+
+# forcibly remove packages to avoid artifacts
+conda uninstall -y --force \
+    metpy \
+    numpy \
+    scipy \
+    dask \
+    distributed \
+    cftime \
+    pint \
+    xarray
+
+# if available install from nightly wheels
+python -m pip install \
+    -i https://pypi.anaconda.org/scipy-wheels-nightly/simple \
+    --no-deps \
+    --pre \
+    --upgrade \
+    numpy \
+    xarray
+
+# install rest from source
+python -m pip install \
+    --no-deps \
+    --upgrade \
+    git+https://github.com/Unidata/MetPy.git \
+    git+https://github.com/dask/dask.git \
+    git+https://github.com/dask/distributed.git \
+    git+https://github.com/hgrecco/pint.git \
+    git+https://github.com/xarray-contrib/cf-xarray.git \
+    git+https://github.com/xarray-contrib/xskillscore.git
