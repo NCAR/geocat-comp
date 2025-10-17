f1a6577ee860bd25d4a2c2bc0d3fd65668ae8833
anissa111 <anissaz@ucar.edu>
2025-10-17 13:17:02 -0600

    also test mixed input types

--- test/test_interpolation.py
+++ test/test_interpolation.py
@@ -53,14 +53,22 @@ class Test_interp_hybrid_to_pressure:
     pres3d = pres3d * 100  # mb to Pa
 
     def test_pressure_at_hybrid_levels(self, p_out, ds_out):
-        # run not officially supported input types w/ hyam and hybm as lists
-        _ = pressure_at_hybrid_levels(
+        # run not officially supported input types w/ hyam and hybm as lists and mixed
+        pm = pressure_at_hybrid_levels(
             self.ps[0, :7, :7], p_out.hyam.values.tolist(), p_out.hybm.values.tolist()
         )
+        nt.assert_allclose(pm, p_out.pm, rtol=1e-6)
+        pm = pressure_at_hybrid_levels(
+            self.ps[0, :7, :7], p_out.hyam.values.tolist(), p_out.hybm
+        )
+        nt.assert_allclose(pm, p_out.pm, rtol=1e-6)
+        pm = pressure_at_hybrid_levels(
+            self.ps[0, :7, :7], p_out.hyam, p_out.hybm.values.tolist()
+        )
+        nt.assert_allclose(pm, p_out.pm, rtol=1e-6)
 
-        # test w/ mismatched dim names
+        # mismatched dim names
         pm = pressure_at_hybrid_levels(self.ps[0, :7, :7], p_out.hyam, p_out.hybm)
-
         nt.assert_allclose(pm, p_out.pm, rtol=1e-6)
 
     def test_interp_hybrid_to_pressure_atmos(self, ds_out) -> None:
